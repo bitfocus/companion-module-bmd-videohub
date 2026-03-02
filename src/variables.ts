5163d81e2916cbf90133c3ea0a9474476b86bfbc
Julian Waller <git@julusian.co.uk>
2026-03-02 23:57:31 +0000

    fix: final 2.0 api

--- src/variables.ts
+++ src/variables.ts
@@ -1,4 +1,4 @@
-import type { CompanionVariableDefinition, InstanceBase } from '@companion-module/base'
+import type { CompanionVariableDefinitions, InstanceBase } from '@companion-module/base'
 import type { VideohubTypes } from './types.js'
 import type { VideohubState } from './state.js'
 import { LOCKSTATES } from './choices.js'
@@ -21,16 +21,17 @@ export type VariablesSchema = {
  * Initialize variables.
  */
 export function initVariables(self: InstanceBase<VideohubTypes>, state: VideohubState): void {
-	const variableDefinitions: CompanionVariableDefinition<VariablesSchema>[] = []
 	const variableValues: Partial<VariablesSchema> = {}
+	const variableDefinitions: CompanionVariableDefinitions<VariablesSchema> = {
+		selected_destination: { name: 'Label of selected destination' },
+		selected_source: { name: 'Label of input routed to selected destination' },
+		selected_queued_source: { name: 'Label of selected source' },
+	}
+	updateSelectedDestinationVariables(state, variableValues)
 
 	for (const input of state.iterateInputs()) {
 		if (input.status != 'None') {
-			variableDefinitions.push({
-				name: `Label of input ${input.id + 1}`,
-				variableId: `input_${input.id + 1}`,
-			})
-
+			variableDefinitions[`input_${input.id + 1}`] = { name: `Label of input ${input.id + 1}` }
 			variableValues[`input_${input.id + 1}`] = input.name
 		}
 	}
@@ -39,79 +40,42 @@ export function initVariables(self: InstanceBase<VideohubTypes>, state: Videohub
 		if (output.status != 'None') {
 			const outputType = output.type === 'primary' ? 'output' : 'monitor'
 
-			variableDefinitions.push({
-				name: `Label of ${outputType} ${output.id + 1}`,
-				variableId: `output_${output.outputId + 1}`,
-			})
-
+			variableDefinitions[`output_${output.outputId + 1}`] = { name: `Label of ${outputType} ${output.id + 1}` }
 			variableValues[`output_${output.outputId + 1}`] = output.name
 
-			variableDefinitions.push({
+			variableDefinitions[`output_${output.outputId + 1}_input`] = {
 				name: `Label of input routed to ${outputType} ${output.id + 1}`,
-				variableId: `output_${output.outputId + 1}_input`,
-			})
-
+			}
 			variableValues[`output_${output.outputId + 1}_input`] = state.getInput(output.route)?.name ?? '?'
 
-			variableDefinitions.push({
+			variableDefinitions[`output_${output.outputId + 1}_input_id`] = {
 				name: `Id of input routed to ${outputType} ${output.id + 1}`,
-				variableId: `output_${output.outputId + 1}_input_id`,
-			})
-
+			}
 			const activeId: number | undefined = state.getInput(output.route)?.id
 			variableValues[`output_${output.outputId + 1}_input_id`] = activeId !== undefined ? activeId + 1 : '?'
 
-			variableDefinitions.push({
+			variableDefinitions[`output_${output.outputId + 1}_lock_state`] = {
 				name: `Lock state of ${outputType} ${output.id + 1}`,
-				variableId: `output_${output.outputId + 1}_lock_state`,
-			})
-
+			}
 			variableValues[`output_${output.outputId + 1}_lock_state`] = LOCKSTATES[output?.lock] ?? '?'
 		}
 	}
 
 	for (const serial of state.iterateSerials()) {
 		if (serial.status != 'None') {
-			variableDefinitions.push({
-				name: `Label of serial port ${serial.id + 1}`,
-				variableId: `serial_${serial.id + 1}`,
-			})
-
+			variableDefinitions[`serial_${serial.id + 1}`] = { name: `Label of serial port ${serial.id + 1}` }
 			variableValues[`serial_${serial.id + 1}`] = serial.name
 
-			variableDefinitions.push({
+			variableDefinitions[`serial_${serial.id + 1}_route`] = {
 				name: `Label of serial routed to serial power ${serial.id + 1}`,
-				variableId: `serial_${serial.id + 1}_route`,
-			})
-
+			}
 			const sourceSerial = state.getSerial(serial.route)
 			variableValues[`serial_${serial.id + 1}_route`] = sourceSerial?.name ?? '?'
 
-			variableDefinitions.push({
-				name: `Lock state of serial ${serial.id + 1}`,
-				variableId: `serial_${serial.id + 1}_lock_state`,
-			})
-
+			variableDefinitions[`serial_${serial.id + 1}_lock_state`] = { name: `Lock state of serial ${serial.id + 1}` }
 			variableValues[`serial_${serial.id + 1}_lock_state`] = LOCKSTATES[serial?.lock] ?? '?'
 		}
 	}
-
-	variableDefinitions.push({
-		name: 'Label of selected destination',
-		variableId: 'selected_destination',
-	})
-
-	variableDefinitions.push({
-		name: 'Label of input routed to selected destination',
-		variableId: 'selected_source',
-	})
-
-	variableDefinitions.push({
-		name: 'Label of selected source',
-		variableId: 'selected_queued_source',
-	})
-
-	updateSelectedDestinationVariables(state, variableValues)
 
 	self.setVariableDefinitions(variableDefinitions)
 	self.setVariableValues(variableValues)
